98246516a464671933deecee156f96c9c9f1f06d
chches <charlycris.che@gmail.com>
2024-02-03 10:58:01 +0000

    First Commit

---
+++ invoices/webapp/controller/App.controller.js
@@ -0,0 +1,32 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/m/MessageToast",
+        "sap/ui/model/json/JSONModel"
+    ],   
+    function (Controller, MessageToast, JSONModel) {
+        "use strict";
+
+        return Controller.extend("chches.invoices.controller.App", {
+            onInit : function( ){
+                this._loadModel();
+            },
+
+            _loadModel : function (){
+                let oData = {
+                    recipient : {
+                        name : "World"
+                    }
+                };
+                let oModel = new JSONModel(oData);
+                this.getView().setModel(oModel, "view");
+            },
+
+            onShowHello : function( ){
+                MessageToast.show("Hello World");
+            }
+
+        });
+
+    }
+);
