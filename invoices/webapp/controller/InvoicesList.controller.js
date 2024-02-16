4a9f4eb6811ed7076c4964a95c124f9d26739f30
chches <charlycris.che@gmail.com>
2024-02-16 07:01:52 +0000

    First Commit

---
+++ invoices/webapp/controller/InvoicesList.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel"
+    ],
+    function(Controller, JSONModel){
+        "use strict";
+
+        return Controller.extend("chches.invoices.controller.InvoicesList",{
+            onInit : function( ){
+                //Creamos un Modelo y lo Publicamos
+                //Definimos el Modelo.
+                let oData = {
+                    usd : "USD",
+                    eur : "EUR"
+                };
+                let oCurrency = new JSONModel(oData);
+                this.getView().setModel(oCurrency, "currency");
+                console.log(oCurrency);
+            }
+        });
+    }
+);
